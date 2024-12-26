5ebbc3bdf020f473c3fa2963c0368ddd05dbad1b
cagla.boynuegri <cagla.boynuegri@scalefocus.com>
2024-12-26 13:31:59 +0300

    changed cloud config property

--- src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
+++ src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
@@ -1,12 +1,10 @@
 package guru.springframework.msscbeerservice.domain;
 
+import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
-import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.*;
+import org.hibernate.dialect.VarcharUUIDJdbcType;
 
-import javax.persistence.*;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.UUID;
@@ -25,7 +23,7 @@ public class Beer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    @Type(type="org.hibernate.type.UUIDCharType")
+    @JdbcType(VarcharUUIDJdbcType.class)
     @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
 
